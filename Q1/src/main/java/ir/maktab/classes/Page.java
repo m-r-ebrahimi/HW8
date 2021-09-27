b7fffedbff10629cae08172e2253d80602c471a0
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-27 10:30:19 +0330

    Complete Page class, and it's method to override turn() method.

--- Q1/src/main/java/ir/maktab/classes/Page.java
+++ Q1/src/main/java/ir/maktab/classes/Page.java
@@ -3,8 +3,15 @@ package main.java.ir.maktab.classes;
 import main.java.ir.maktab.classes.Turner;
 
 public class Page implements Turner {
+    private int page;
+
+    public Page(int page) {
+        this.page = page;
+    }
+
     @Override
     public void turn() {
-
+        System.out.println("Going to next page!");
+        System.out.println(page + " -> " + (page + 1));
     }
 }
