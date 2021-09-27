4c082658a6684bfe7d3702317f70157c586c5e0f
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-27 10:36:24 +0330

    Done.

--- Q1/src/main/java/ir/maktab/Main/DemoTurners.java
+++ Q1/src/main/java/ir/maktab/Main/DemoTurners.java
@@ -1,4 +1,18 @@
 package main.java.ir.maktab.Main;
 
+import main.java.ir.maktab.classes.Leaf;
+import main.java.ir.maktab.classes.Page;
+import main.java.ir.maktab.classes.Pancake;
+
 public class DemoTurners {
+    public static void main(String[] args) {
+        Leaf leaf = new Leaf("Red", "Blue");
+        Page page = new Page(10);
+        Pancake pancake = new Pancake();
+
+        leaf.turn();
+        page.turn();
+        page.turn();
+    }
+
 }
