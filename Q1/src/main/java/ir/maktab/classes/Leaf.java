ed12a9147d9cd9f4524c9b01b58dea319b6c0bcf
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-27 10:26:44 +0330

    Complete Leaf class, and it's method to override turn() method.

--- Q1/src/main/java/ir/maktab/classes/Leaf.java
+++ Q1/src/main/java/ir/maktab/classes/Leaf.java
@@ -3,9 +3,17 @@ package main.java.ir.maktab.classes;
 import main.java.ir.maktab.classes.Turner;
 
 public class Leaf implements Turner {
+    private String firstColor;
+    private String secondColor;
+
+    public Leaf(String firstColor, String secondColor) {
+        this.firstColor = firstColor;
+        this.secondColor = secondColor;
+    }
 
     @Override
     public void turn() {
-
+        System.out.println("Changing color!");
+        System.out.println(firstColor + " changed to " + secondColor);
     }
 }
