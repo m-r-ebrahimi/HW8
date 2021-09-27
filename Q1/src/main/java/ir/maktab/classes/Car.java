2e21cd9b2474b1a4e4913a5d922b527d41ce26c7
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-27 10:43:36 +0330

    Complete Car class, and it's method to override turn() method.

---
+++ Q1/src/main/java/ir/maktab/classes/Car.java
@@ -0,0 +1,10 @@
+package main.java.ir.maktab.classes;
+
+public class Car implements Turner {
+
+
+    @Override
+    public void turn() {
+        System.out.println("Turn left to right.");
+    }
+}
