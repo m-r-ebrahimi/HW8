f184943cf58a230089e45b70840fa6919eacd91f
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-28 15:54:57 +0330

    Initial classes to rethrow exception.

---
+++ Q3/src/main/java/ir/maktab/Main.java
@@ -0,0 +1,29 @@
+package ir.maktab;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] str = null;
+        //String[] str = {"ali", "reza"};
+        try {
+            demo(str);
+        } catch (NullPointerException ne) {
+            System.out.println("Catch null exception in main.");
+            ne.printStackTrace();
+        }
+    }
+
+    public static void demo(String... str) {
+        try {
+            if (str == null)
+                throw new NullPointerException("null!");
+            else
+                System.out.println(Arrays.toString(str));
+        } catch (NullPointerException ne) {
+            System.out.println("Catch null exception in method.");
+            ne.printStackTrace();
+            throw ne;
+        }
+    }
+}
